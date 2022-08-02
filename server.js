6b2627cb3c193b52a1558d28d8bc5971aa011b19
grecuadina <adina@programmer.net>
2022-08-02 14:30:21 +0300

    get and post requests

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 
 const express = require("express");
+const bodyparser = require("body-parser");
 const { Sequelize, DataTypes } = require('sequelize');
 const app = express();
 const port = 1234;
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.get('', (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
 
 
 
@@ -49,128 +55,131 @@ sequelize.authenticate().then(
 
         Operation.sync().then(() => {
 
-            //add
-
-            app.get('/add/:nr1(\\d+)/:nr2(\\d+)', (req, res) => {
-
-                const n1 = +req.params.nr1;
-                const n2 = +req.params.nr2;
-                const sum = (n1 + n2).toString();
-
-                if (sum == 'NaN') {
-                    res.statusMessage = "Bad Request";
-                    res.status(400).end();
-                } else {
-                    res.send(sum);
-                    Operation.create({ op: "add", a: n1, b: n2, result: sum }).then(
-                        (add) => {
-                            console.log("Adunare cu rezultatul " + add.result);
-                        }
-                    );
-                }
+            app.post('/', (req, res) => {
+                
+                const button = req.body.op;
+                const n1 = +req.body.nr1;
+                const n2 = +req.body.nr2;
+                
+                if(button == "ADD"){
+                    const add = (n1 + n2).toString();
+                    if (add == 'NaN') {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).end();
+                    } else {
+                        res.send(add);
+                        Operation.create({ op: "add", a: n1, b: n2, result: add }).then(
+                            (add) => {
+                                console.log("Adunare cu rezultatul " + add.result);
+                            }
+                        );
+                    }
+                } else if(button == "SUBSTRACT"){
+                    const sub = (n1 - n2).toString();
+                    if (sub == 'NaN') {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).end();
+                    } else {
+                        res.send(sub);
+                        Operation.create({ op: "sub", a: n1, b: n2, result: sub }).then(
+                            (sub) => {
+                                console.log("Scadere cu rezultatul " + sub.result);
+                            }
+                        );
+                    }
+                } else if(button == "MULTIPLY"){
+                    const mul = (n1 * n2).toString();
+                    if (mul == 'NaN') {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).end();
+                    } else {
+                        res.send(mul);
+                        Operation.create({ op: "mul", a: n1, b: n2, result: mul }).then(
+                            (mul) => {
+                                console.log("Inmultire cu rezultatul " + mul.result);
+                            }
+                        );
+                    }
+                } else if(button == "DIVIDE"){
+                    const div = (n1 / n2).toString();
+                    if (div == 'NaN') {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).end();
+                    } else {
+                        res.send(div);
+                        Operation.create({ op: "div", a: n1, b: n2, result: div }).then(
+                            (div) => {
+                                console.log("Impartire cu rezultatul " + div.result);
+                            }
+                        );
+                    }
+                };
+           
             });
 
-            //substract
+            //clear
+            app.get('/memory/clear', (req, res) => {
 
-            app.get('/substract/:nr1(\\d+)/:nr2(\\d+)', (req, res) => {
+                Operation.destroy({
+                    where: {}
+                });
 
-                const n1 = +req.params.nr1;
-                const n2 = +req.params.nr2;
-                const dif = (n1 - n2).toString();
-            
-                if (dif == 'NaN') {
-                    res.statusMessage = "Bad Request";
-                    res.status(400).end();
-                } else {
-                    res.send(dif);
-                    Operation.create({ op: "dif", a: n1, b: n2, result: dif }).then(
-                        (dif) => {
-                            console.log("Scadere cu rezultatul " + dif.result);
-                        }
-                    );
-                }
+                res.send('I remember nothing.');
+                console.log("Memory clear");
             });
 
-            //mul
 
-            app.get('/multiply/:nr1(\\d+)/:nr2(\\d+)', (req, res) => {
+            //recall
 
-                const n1 = +req.params.nr1;
-                const n2 = +req.params.nr2;
-                const mul = (n1 * n2).toString();
             
-                if (mul == 'NaN') {
-                    res.statusMessage = "Bad Request";
-                    res.status(400).end();
-                } else {
-                    res.send(mul);
-                    Operation.create({ op: "mul", a: n1, b: n2, result: mul}).then(
-                        (mul) => {
-                            console.log("Inmultire cu rezultatul " + mul.result);
-                        }
-                    );
-                }
+            app.get('/memory/recall',  (req, res) => {
+
+                const last = Operation.findOne({ 
+                    order: [
+                        ['createdAt', 'DESC']
+                    ],
+                 },).then((rez) => {
+                    if(rez){
+                        res.send(`Last result was ${rez.result}`);
+                    console.log("Memory recall");
+                }else{
+                    res.send("Nothing to see here");
+                    console.log("Memory recall failed");
+                };
+                    
+                },
+                 (error) => {
+                    console.error('Unable to recall: ', error);
+                 }
+                 );      
+
             });
-
-            //div
-
-            app.get('/divide/:nr1(\\d+)/:nr2(\\d+)', (req, res) => {
-
-                const n1 = +req.params.nr1;
-                const n2 = +req.params.nr2;
-                const div = (n1 / n2).toString();
-            
-                if (div == 'NaN' || div == 'Infinity') {
-                    res.statusMessage = "Bad Request";
-                    res.status(400).end();
-                } else {
-                    res.send(div);
-                    Operation.create({ op: "div", a: n1, b: n2, result: div}).then(
-                        (div) => {
-                            console.log("Impartire cu rezultatul " + div.result);
-                        }
-                    );
-                }
-            });
-
-            
-
-
         });
 
     },
-    (error) => {
-        console.error('Unable to connect to the database:', error);
+(error) => {
+    console.error('Unable to connect to the database:', error);
 
-    }
+}
 );
 
 
-app.get('/memory/clear', (req, res) => {
 
-    vector = [];
-    res.send('I remember nothing.');
-    console.log(vector);
-});
 
-app.get('/memory/recall', (req, res) => {
-    if (vector.length === 0) res.send("Nothing here.");
-    else res.send("The last thing I remember is " + vector[vector.length - 1].result) + ".";
-    console.log(vector);
-});
 
-app.get('/memory/recall/:pos(\\d+)', (req, res) => {
-    const position = +req.params.pos;
-    const len = vector.length;
-    if (len < req.params.pos) {
-        res.send("You expect too much from me.");
-    } else {
-        res.send(`I recall the result in position ${position} is ${vector[len - position].result}.`);
-    }
 
-});
+// app.get('/memory/recall/:pos', (req, res) => {
+//     const position = +req.params.pos;
+//     const len = vector.length;
+//     if (len < req.params.pos) {
+//         res.send("You expect too much from me.");
+//     } else {
+//         res.send(`I recall the result in position ${position} is ${vector[len - position].result}.`);
+//     }
+
+// });
 
 
 app.listen(port, () => {
-    console.log('server is up')
+    console.log('server is up');
 });
